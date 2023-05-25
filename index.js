305eeef10b77033ba593a8398405c4b39f1d6cda
subrat199 <subratt01998@gmail.com>
2023-05-25 12:44:43 +0530

    jsonserver deploy

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const jsonServer = require('json-server')
-const cors = require('cors')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-// server.use(cors())
-// server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
-server.use(cors())
-const PORT = 8000
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
+server.listen(8000, () => {
+  console.log('JSON Server is running')
 })
