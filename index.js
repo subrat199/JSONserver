3f24b693f616bff8c1e647ca75df3f0bac9a6b70
subrat199 <subratt01998@gmail.com>
2023-05-25 13:49:24 +0530

    deploy new

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-const PORT=8000
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middleware = jsonServer.defaults();
+const port = process.env.PORT || 8000;
 
-server.use(middlewares)
-server.use(router)
-server.listen(PORT)
+server.use(middleware);
+server.use(router);
+
+server.listen(port);
