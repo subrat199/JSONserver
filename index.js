4ce175f11031267d74c05e540ef5be318170d058
subrat199 <subratt01998@gmail.com>
2023-05-25 12:40:56 +0530

    jsonserver deploy

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const jsonServer = require('json-server')
 const cors = require('cors')
-const path = require('path')
-
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 // server.use(cors())
 // server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
-
+server.use(cors())
 const PORT = 8000
 
 server.listen(PORT, () => {
