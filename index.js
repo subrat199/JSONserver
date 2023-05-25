9c05eec398fe21ccaf20357dc16c3ee04de23d4f
subrat199 <subratt01998@gmail.com>
2023-05-25 13:31:02 +0530

    deploy for server

--- index.js
+++ index.js
@@ -2,14 +2,8 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const PORT=8000
 
 server.use(middlewares)
 server.use(router)
-// server.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'https://lime-panicky-seahorse.cyclic.app')
-//     res.header('Access-Control-Allow-Headers', '*')
-//     next()
-//   })
-server.listen(8000, () => {
-  console.log('JSON Server is running')
-})
+server.listen(PORT)
