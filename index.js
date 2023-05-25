adc21b3a9ecb808480eaab77fa36edeb476f4791
subrat199 <subratt01998@gmail.com>
2023-05-25 12:54:59 +0530

    jsonserver deploy

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://lime-panicky-seahorse.cyclic.app')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+  })
 server.listen(8000, () => {
   console.log('JSON Server is running')
 })
